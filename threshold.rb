76dbd32da9a69f12afdfa4d4737963ff58468b65
Akira FUNAI <funai@mac-mini.local>
2016-10-29 15:15:48 +0900

    h56x28a

--- threshold.rb
+++ threshold.rb
@@ -1,18 +1,18 @@
 require 'rmagick'
 
-img = Magick::ImageList.new('grade_85lines.png').first
+img = Magick::ImageList.new('grade_60lines.png').first
 
 t = []
-20.times {|i| t[i] = []}
+56.times {|i| t[i] = []}
 
 level = 1
 
-256.times.each do |box|
+600.times.each do |box|
   increment = false
-  20.times do |y|
-    20.times do |x|
-p '!!!!!!' if img.pixel_color(x, y + box * 20).to_color == 'white' && t[x][y]
-      if img.pixel_color(x, y + box * 20).to_color == 'black' && t[x][y].nil? then
+  28.times do |y|
+    56.times do |x|
+p '!!!!!!' if img.pixel_color(x, y + box * 28).to_color == 'white' && t[x][y]
+      if img.pixel_color(x, y + box * 28).to_color == 'black' && t[x][y].nil? then
         t[x][y] = level
         increment = true
       end
@@ -34,14 +34,14 @@ puts <<_EOS
 <!ATTLIST levels divisor CDATA #REQUIRED>
 ]>
 <thresholds>
-  <threshold map="h20x20a">
-    <description>Halftone 20x20 (angled)</description>
-    <levels width="20" height="20" divisor="#{level}">
+  <threshold map="h56x28a">
+    <description>Halftone 56x28 (angled)</description>
+    <levels width="56" height="28" divisor="#{level}">
 _EOS
 
-20.times do |y|
+28.times do |y|
   print '     '
-  20.times do |x|
+  56.times do |x|
     printf ' %3.3s', t[x][y].to_i
   end
   puts
